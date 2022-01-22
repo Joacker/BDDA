dad32a6a3a4aa39dd2cec867f3b061299d0eb684
Joaquín Fernández <67400456+Joako-Sador@users.noreply.github.com>
2022-01-21 22:35:44 -0300

    Update cmpt.sh

--- Actividades/Actividad 2/cmpt.sh
+++ Actividades/Actividad 2/cmpt.sh
@@ -1,9 +1,8 @@
 #Creacion de la tabla temporal, para los ruts que se han de ingresar.
-2 create temporary table ruts (rut numeric(10));
-3 #Se copian los valores del archivo 10mil a la tabla temporal
-4 #creada anteriormente.
-5 copy ruts from ’/home/joaquin/BDDA/BDDA2/diezmil’;
-6 #Por ultimo se recuperan los valores que han de corresponder
-7 #con respecto a PERSONAS1
-8 \copy (select personas1.direccion from personas1, ruts
-9 where personas1.rut = ruts.rut) to ’/tmp/savep1.txt’;
+create temporary table ruts (rut numeric(10));
+#Se copian los valores del archivo 10mil a la tabla temporal
+#creada anteriormente.
+copy ruts from ’/home/joaquin/BDDA/BDDA2/diezmil’;
+#Por ultimo se recuperan los valores que han de corresponder
+#con respecto a PERSONAS1
+\copy (select personas1.direccion from personas1, ruts where personas1.rut = ruts.rut) to ’/tmp/savep1.txt’;
